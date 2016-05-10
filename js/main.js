ae30150de59cc388997bca5e82bd97f58d44d51c
Dominic Muttel <dominic.muttel@gmail.com>
2016-05-10 01:03:22 -0600

    nav touch up

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@
 
 $(document).on('ready', function() {
   console.log('sanity check!');
+  var blurb = $('<div id="blurb"><p>OUTDOOR FESTIVAL OF DIGITAL ANIMATION AND ART<br>DOWNTOWN DENVER SATURDAY SEPTEMBER 24TH</p></div>');
+   var blurbSm = $('<div><p>OUTDOOR FESTIVAL OF DIGITAL ANIMATION AND ART DOWNTOWN DENVER SATURDAY SEPTEMBER 24TH</p></div>');
+  $(blurb).appendTo(".navbar-header");
+  $(blurbSm).appendTo("#blurb-area");
+
 });

4c309d941db27d3e3238cf12af3f674779f5e3ea
Dominic Muttel <dominic.muttel@gmail.com>
2016-08-22 22:56:50 -0600

    major update

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,123 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var connect = require('gulp-connect');
-var uglify = require('gulp-uglify');
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    cssmin = require('gulp-cssmin'),
+    maps = require('gulp-sourcemaps'),
+    del = require('del'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create(),
+// need to work in flow
+    handlebars = require('gulp-compile-handlebars'),
+// end
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish');
 
-// configure connect task
-gulp.task('connect', function() {
-  connect.server({
-    livereload: true
-  });
-});
 
-gulp.task('html', function () {
-  gulp.src('*.html')
-    .pipe(connect.reload());
-});
 
-gulp.task('css', function () {
-  gulp.src('css/*.css')
-    .pipe(connect.reload());
-});
 
+// auto prefix vendors
+var AUTOPREFIXER_BROWSERS = [
+    'ie >= 10',
+    'ie_mob >= 10',
+    'ff >= 30',
+    'chrome >= 34',
+    'safari >= 7',
+    'opera >= 23',
+    'ios >= 7',
+    'android >= 4.4',
+    'bb >= 10'
+];
 // configure jshint task
 gulp.task('jshint', function() {
-  return gulp.src('js/main.js')
+  return gulp.src('js/*.js')
     .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
+    .pipe(jshint.reporter('stylish'));
+});
+//=======================================
+// concat all .js
+// js/main.js -> production/app.js
+//=======================================
+gulp.task('concatScripts', function(){
+  return gulp.src(['js/main.js', 'js/map.js']) // <- add other js files in array
+  //concats files to one file
+  .pipe(maps.init())
+  .pipe(concat('app.js'))
+  .pipe(maps.write('../maps')) // source map for debugging
+  .pipe(gulp.dest('js'))
 });
 
-// configure which files to watch and what tasks to use on file changes
-gulp.task('watch', function() {
-  gulp.watch('js/*.js', ['jshint']);
-  gulp.watch(['*.html'], ['html']);
-  gulp.watch(['css/*.css'], ['css']);
+//=======================================
+// minify all .js
+// production/app.js -> to production/app.min.js
+//=======================================
+gulp.task('minifyScripts', ['concatScripts'], function(){
+  return gulp.src('js/app.js')
+  // minifies code
+  .pipe(uglify())
+  // rename file to app.min.js
+  .pipe(rename('app.min.js'))
+  .pipe(gulp.dest('js'))
 });
 
-// configure minify
-gulp.task('compress', function() {
-  return gulp.src('lib/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist'));
+//=======================================
+// sass -> css
+//=======================================
+
+gulp.task('compileSass', function(){
+    return gulp.src('scss/**/*.scss')
+    .pipe(maps.init())
+    .pipe(sass())
+	.pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+    .pipe(maps.write('../maps')) // source map for debugging
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream())
 });
+
+gulp.task('minifyCss', ['compileSass'] function () {
+    return gulp.src('css/app.css')
+    .pipe(cssmin())
+    .pipe(rename('app.min.css'))
+    .pipe(gulp.dest('css'));
+});
+
+//=======================================
+// watch sass for any changes
+//=======================================
+gulp.task('watchFiles', function(){
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch('scss/**/*.scss', ['compileSass']);
+    gulp.watch('production/app.js', ['concatScripts'], browserSync.reload);
+    gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch('js/*.js').on('change', browserSync.reload);
+
+});
+
+//=======================================
+// clean out old compiled pages
+//=======================================
+gulp.task('clean', function(){
+    del(['dist', 'css', 'production']);
+});
+
+//=======================================
+// build production dirrectory
+//=======================================
+gulp.task('build', ['minifyScripts', 'minifyCss'], function(){
+    return gulp.src(['css/app.min.css', 'js/app.min.js', 'index.html', 'images/**', 'fonts/**'], { base: './' })
+    .pipe(gulp.dest('dist'))
+});
+
+
+gulp.task('serve', ['watchFiles']);
+
 
 // default task!
-gulp.task('default', ['watch', 'connect']);
+gulp.task('default', ['clean'], function(){
+    gulp.start('build');
+});

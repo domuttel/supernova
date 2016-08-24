74505399012567813801acdc29b924827f3c050d
Dominic Muttel <dominic.muttel@gmail.com>
2016-08-24 09:03:28 -0600

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).on('ready', function() {
-    var blurb = $('<div id="blurb"><p>OUTDOOR FESTIVAL OF DIGITAL ANIMATION AND ART<br>DOWNTOWN DENVER SATURDAY SEPTEMBER 24TH</p></div>');
-    var blurbSm = $('<div><p>OUTDOOR FESTIVAL OF DIGITAL ANIMATION AND ART DOWNTOWN DENVER SATURDAY SEPTEMBER 24TH</p></div>');
-    $(blurb).appendTo(".navbar-header");
-    $(blurbSm).appendTo("#blurb-area");
+    // var blurb = $('<div id="blurb"><p>OUTDOOR FESTIVAL OF DIGITAL ANIMATION AND ART<br>DOWNTOWN DENVER SATURDAY SEPTEMBER 24TH</p></div>');
+    // var blurbSm = $('<div><p>OUTDOOR FESTIVAL OF DIGITAL ANIMATION AND ART DOWNTOWN DENVER SATURDAY SEPTEMBER 24TH</p></div>');
+    // $(blurb).appendTo(".navbar-header");
+    // $(blurbSm).appendTo("#blurb-area");
     $('.carousel').carousel({
         interval: 3000,
     });

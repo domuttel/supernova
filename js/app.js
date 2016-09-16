85c00b4c291ba5937474fd002d08f6d28405a385
Dominic Muttel <dominic.muttel@gmail.com>
2016-09-16 05:42:03 -0600

    it for now

--- js/app.js
+++ js/app.js
@@ -1,3 +1,74 @@
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("mobile-e");
+var btn2 = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+    modal.style.zIndex = "9999";
+    // modal.css("z-index", "2000");
+}
+btn2.onclick = function() {
+    modal.style.display = "block";
+    modal.style.zIndex = "9999";
+    // modal.css("z-index", "2000");
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+    modal.style.zIndex = "-9999";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+// var video = document.getElementById("videobg");
+// function enterSp(){
+//     video.hide();
+// }
+
+// function pauseAt(){
+//     video.addEventListener("timeupdate", function(){
+//         if(this.currentTime >= 29) {
+//             this.pause();
+//         }
+//     });
+// };
+// video.autoplay = true;
+// video.muted = true;
+// video.autoloop = true;
+
+// video.onloadstart = function() {
+//     video.currentTime = 1;
+//     video.play();
+//     if (video.end === true){
+//     }
+// };
+
+// function vidplay() {
+//     video.loop=true;
+//     if (video.paused) {
+//         video.play();
+//     } else {
+//         video.pause();
+//     }
+// }
+
+// function restart() {
+//     video.currentTime = 1;
+//     video.play();
+//     pauseAt(video);
+// }
+
 $(document).ready( function() {
     $('#nav-command').on('click','#about-button', function(){
         $('#about-list').fadeIn();
@@ -29,13 +100,34 @@ $(document).ready( function() {
     //         $('#logo-banner').css("font-size", "3.3rem");
     //     }
     // });
-
+// var jurors = $(".juror-img");
+// $('# div').on('click', function() {
+//         var index = $(this).index();
+//         if (index === 0) {
+//             screenings.load('pages/jurors.html');
+//         }
+//         else if (index === 1) {
+//             screenings.load('pages/jurors.html');
+//         }
+//         else {
+//             screenings.load('pages/jurors.html');
+//         }
+// };
+var container = $('#loaded-content');
+$('.juror-img').on('click', function() {
+    var index = $(this).index();
+    container.empty( function(){
+    });
+    container.fadeIn(1000).load('pages/jurors.html');
+    allSched.hide();
+    home.hide();
+});
+var screenings = $("#screenings");
     var home = $("#home");
-    var screenings = $("#screenings");
     var education = $('#education');
     var maps = $("#maps");
     var allSched = $(".sched-containers");
-    var container = $('#loaded-content');
+
 
     $('#logo-banner').on('click', function() {
         container.empty();
@@ -46,6 +138,24 @@ $(document).ready( function() {
         var index = $(this).index();
         container.empty();
         $('.nav-list').fadeOut();
+        home.hide();
+        allSched.hide();
+        if (index === 0) {
+            screenings.fadeIn(1000);
+        }
+        else if (index === 1) {
+            education.fadeIn(1000);
+        }
+        else{
+            maps.fadeIn(1000);
+        }
+    });
+    $('#mobile-sched-list div').on('click', function() {
+        var index = $(this).index();
+        container.empty();
+        hamburger.toggleClass('active');
+            $('#mobile-menu').toggle();
+        // $('.nav-list').fadeOut();
         home.hide();
         allSched.hide();
         if (index === 0) {
@@ -86,11 +196,87 @@ $(document).ready( function() {
             container.fadeIn(1000).load('pages/guidelines.html');
         }
     });
+    $('#mobile-about-list div').on('click', function() {
+        var index = $(this).index();
+        home.hide();
+        allSched.hide();
+        container.hide().empty();
+        hamburger.toggleClass('active');
+            $('#mobile-menu').toggle();
+        // $('.nav-list').fadeOut();
+        if (index === 0) {
+            container.fadeIn(1000).load('pages/festival.html');
+        }
+        else if (index === 1) {
+            container.fadeIn(1000).load('pages/jurors.html');
+        }
+        else if (index === 2) {
+            container.fadeIn(1000).load('pages/the-team.html');
+        }
+        else if (index === 3) {
+            container.fadeIn(1000).load('pages/history.html');
+        }
+        else if (index === 4) {
+            container.fadeIn(1000).load('pages/press.html');
+        }
+        else if (index === 5) {
+            container.fadeIn(1000).load('pages/sponsors.html');
+        }
+        else {
+            container.fadeIn(1000).load('pages/guidelines.html');
+        }
+    });
+
+    // $('#mobile-sched-list div').on('click', function() {
+    //     var index = $(this).index();
+    //     container.empty();
+    //     home.hide();
+    //     hamburger.toggleClass('active');
+    //     $('#mobile-menu').toggle();
+    //     // allSched.hide();
+    //     if (index === 0) {
+    //         screenings.fadeIn(1000);
+    //     }
+    //     if (index === 1) {
+    //         console.log(index);
+    //         // console.log("bbb");
+    //         // $('#mobile-menu').fadeOut();
+    //         education.fadeIn(1000);
+    //     }
+    //     else{
+    //         // $('#mobile-menu').fadeOut();
+    //         maps.fadeIn(1000);
+    //     }
+    // });
     var hamburger = $('#hamburger-icon');
-      hamburger.click(function() {
-         hamburger.toggleClass('active');
-         return false;
-      });
+    var mobile = $('#mobile-menu');
+    hamburger.click(function() {
+        hamburger.toggleClass('active');
+        mobile.toggle('slow');
+        return false;
+        // mobile.animate({top: "+=100"});
+    });
+    var $contactForm = $('#my-form');
+    $contactForm.submit(function(e) {
+    	e.preventDefault();
+    	$.ajax({
+    		url: '//formspree.io/ivar@plusgallery.com',
+    		method: 'POST',
+    		data: $(this).serialize(),
+    		dataType: 'json',
+    		beforeSend: function() {
+    			$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+    		},
+    		success: function(data) {
+    			$contactForm.find('.alert--loading').hide();
+    			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+    		},
+    		error: function(err) {
+    			$contactForm.find('.alert--loading').hide();
+    			$contactForm.append('<div class="alert alert--error">Oops, there was an error.</div>');
+    		}
+    	});
+    });
 });
 
 //# sourceMappingURL=../maps/app.js.map
